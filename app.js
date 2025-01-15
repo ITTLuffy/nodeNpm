27b6c8e13bce2e47bec120ef8cf6e201e928dfde
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2025-01-15 12:48:45 +0100

    FEAT: add js

--- app.js
+++ app.js
@@ -8,5 +8,32 @@ const item = document.createElement('li'); // nuovo elemento da aggiungere
 // item.textContent = 'Contacts';
 // item.innerText;
 
-item.innerHTML = <a href="#">Contacts</a>;
+item.innerHTML = '<a href="#">Contacts</a>';
 lista.appendChild(item); // lo aggiungo 
+
+const article = {
+    type: 'article',
+    children: [
+        {
+            type: 'h2',
+            children: 'Article Title'
+        }, 
+        {
+            type: 'p',
+            children: 'Article content'
+
+        }
+
+    ]
+    
+    // array di children
+
+}
+
+const rootContent = document.querySelector('#content');
+
+createContentent(article, rootContent);
+
+const newItem = document.createElement('article');
+
+
